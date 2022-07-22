9d3fad5eb9c41ce5264a0c9f7a8055923856fae0
akashpradeesh05@gmail.com <akashpradeesh05@gmail.com>
2022-07-22 12:27:24 +0530

    commit

--- src/Introduction.js
+++ src/Introduction.js
@@ -3,12 +3,12 @@ import Login from './Login'
 import SignUp from'./SignUp';
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
+// import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+// import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
